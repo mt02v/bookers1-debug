e8e2fb1ff35d142177191c951b8d567d911e316c
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-27 06:27:24 +0000

    [Finish] NoMethodError undefined method 'title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_061455) do
+ActiveRecord::Schema.define(version: 2023_09_27_061716) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
